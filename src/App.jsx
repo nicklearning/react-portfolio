982826bfe5f2a846e68716223c87f8b7b87f9791
Nick Smith <nick.smith.asu@gmail.com>
2024-03-21 18:33:59 -0700

    Add about me content

--- src/App.jsx
+++ src/App.jsx
@@ -8,11 +8,11 @@ function App() {
     // The Outlet component will conditionally swap between the different pages according to the URL
     return (
         <>
-            <Header />
-            <main className="mx-3">
+            <main>
+                <Header />
                 <Outlet />
+                <Footer />
             </main>
-            <Footer />
         </>
     );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,24 @@
 export default function About() {
-    return (
-      <div>
-        <h1>About Page</h1>
+  return (
+    <div className="mx-4 py-4 about-me-container">
+      <h3>About Me</h3>
+      <div className="profile-picture-container">
+        <img src="./src/assets/profile-pic.jpg" alt="Profile" className="profile-picture" />
+      </div>
+      <div className="py-3">
         <p>
-          Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-          Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-          mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-          lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-          imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-          in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+          Hello and welcome to my portfolio page! My name is Nick Smith, and I am passionate about embarking on a fulfilling career as a full stack web developer. Hailing from the sunny city of Scottsdale, Arizona, I bring with me a blend of dedication, enthusiasm, and a strong foundation in React.
+        </p>
+        <p>
+          Driven by a desire to craft immersive digital experiences, I thrive on challenging projects and am constantly seeking to expand my skills in web development. My proficiency in React allows me to create dynamic and responsive user interfaces that elevate the browsing experience.
+        </p>
+        <p>
+          Outside of the digital realm, you can often find me indulging in activities that rejuvenate the mind and body. Whether it's hitting the greens for a round of golf, immersing myself in a captivating book, finding peace through meditation, or exploring the beauty of nature, I believe in fostering a balanced lifestyle that fuels both creativity and personal growth.
+        </p>
+        <p>
+          With a strong work ethic and a passion for continuous learning, I am excited to collaborate on innovative projects and contribute positively to the world of web development. Thank you for taking the time to learn a bit about me, and I look forward to connecting with you further.
         </p>
       </div>
-    );
-  }
-  
+    </div >
+  );
+}
