1b6d60221982d5ef9d90ba8fd70b266e7d4f5e61
Nick Smith <nick.smith.asu@gmail.com>
2024-04-05 14:47:39 -0700

    Create the Contact Page

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,16 @@
+
+const Contact = () => {
+    return (
+        <div className="contact-container">
+            <h2>Contact</h2>
+            <p>Welcome! Let's Connect.</p>
+            <p>
+                Are you interested in collaborating on a project, discussing potential opportunities, or simply want to say hello? I'm always eager to connect with fellow creatives, entrepreneurs, and enthusiasts alike. Whether you have a question, an idea you'd like to explore together, or just want to chat about the latest trends in our field, don't hesitate to reach out. Let's start a conversation and see where it takes us.
+            </p>
+            <p className="contact-info">Email: nick.smith.asu@gmail.com</p>
+            <p className="contact-info" >Phone: 480-544-3595</p>
+        </div>
+    );
+};
+
+export default Contact;
