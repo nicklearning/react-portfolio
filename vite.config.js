ca0f50f75cb9425c3205b5afc443056ae5e98126
Nick Smith <nick.smith.asu@gmail.com>
2024-04-08 11:24:30 -0700

    Edit Vite Build Config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    assetsDir: 'assets', // Specify the directory for assets within the output directory
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'), // Entry HTML file
+      },
+    },
+  },
+});
